e74f0e1361c01b7ace74bbad7206b7f2ea9b349e
rhoeunbin <rhoeb21@gmail.com>
2024-05-22 01:46:35 +0900

    [feat] Initial commit

---
+++ src/test/java/com/rhonnbin/companyapp/CompanyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhonnbin.companyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
